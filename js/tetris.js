6d12a31fb8b946e97d67497a9a03257402180917
Kenta  Shiotani <231205094@ccmailg.meijo-u.ac.jp>
2026-05-18 13:55:40 +0900

    tetris 色の変化

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'lime', 'purple'
+    'cyan', 'orange', 'teal', 'yellow', 'tomato', 'lime', 'indigo'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
