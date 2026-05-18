ae791540bfe2a2d0145e86e298cf19092deecf0b
Kenta  Shiotani <231205094@ccmailg.meijo-u.ac.jp>
2026-05-18 14:10:53 +0900

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -199,8 +199,7 @@ function newGame() {
     init();
     newShape();
     lose = false;
-    interval = setInterval( tick, 10
-        00 );
+    interval = setInterval( tick, 1000 );
 }
 
 function clearAllIntervals(){
